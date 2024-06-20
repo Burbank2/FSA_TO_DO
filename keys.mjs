161d79f3247a0c65de67a4857f1ac1f4acc27cdb
Michael Burbank <burbank2@yahoo.com>
2024-06-20 15:19:16 -0500

    Update UI colors, update input borders, placeholder, input focus

---
+++ keys.mjs
@@ -0,0 +1,18 @@
+
+// ENTER KEY
+// export function enterKey(event) {  
+//     if (event.keyCode === 13) {
+// 		// Prevent form submission
+//         event.preventDefault(); 
+//         addTask();
+//     };
+// }
+
+// // DOWN ARROW KEY
+// export function downArrowKey(event){
+//     if (event.keyCode === 40) {
+// 		// Prevent form submission
+//         event.preventDefault(); 
+//         addTask();
+//     };
+// }
--- script.js
+++ script.js
@@ -1,21 +1,53 @@
-const inputBox = document.getElementById("input-box");
+// import { enterKey, downArrowKey } from "./keys.js"
+
+
 const listContainer = document.getElementById("li-container");
+const inputBox = document.getElementById('input-box');
+const inputDate = document.getElementById('inputDate');
+const modal = document.getElementById("alertModal");
+const span = document.getElementsByClassName("close")[0];
 
 addTask = () => {
-	if (inputBox.value === "") {
-		alert("Please enter a task");
-	} else {
-		let li = document.createElement("li");
-		li.innerHTML = inputBox.value;
-		listContainer.appendChild(li);
+    if (inputBox.value === "" || inputDate.value === "") {
+        modal.style.display = "block";
+		
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = `${inputBox.value} - Due: ${inputDate.value}`;
+        listContainer.appendChild(li);
 
-		let span = document.createElement("span");
-		span.innerHTML = "\u00d7";
-		li.appendChild(span);
-	}
-	inputBox.value = "";
-	saveData();
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    inputDate.value = "";
+    
+    saveData();
+    
+    
 };
+
+
+// enterKey = (event) => {  
+//     if (event.keyCode === 13) {
+// 		// Prevent form submission
+//         event.preventDefault(); 
+//         addTask();
+//     };
+// }
+
+
+
+
 
 listContainer.addEventListener(
 	"click",
@@ -39,5 +71,5 @@ function saveData() {
 function showTask() {
 	listContainer.innerHTML = localStorage.getItem("data");
 }
-
+// enterKey();
 showTask();
